5cf7e3f75265189dc05f33f1a0f0084a066598f4
Jackie Roush <kroush@wisc.edu>
2017-04-26 16:50:14 -0500

    restart

--- www/js/app.js
+++ www/js/app.js
@@ -1,71 +1,85 @@
-// 407 Video App
-// Authors: Roush, Joo, Wu, Crews-hill
-// Code used and modified from Frank Reding(Neontribe.co.uk)
+// Ionic Starter App
 
-var videoApp = angular.module('videoApp', ['ionic', 'ngCordova'])
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-videoApp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
-  if(window.plugins && window.plugins.nativepagetransitions)
-    $ionicConfigProvider.views.transition('none');
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
-    .state('startPage',{
-      url: '/',
-      templateUrl: '/index.html',
-      controller: 'startPage'
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
     })
-    .state('takeAVideoPage', {
-
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
     })
 
-  $urlRouterProvider.otherwise("/");
-})
-
-videoApp.controller('startPage', function($scope, $cordovaCapture, $state, $cordovaFile, $cordovaMedia){
-  //This is for the home screen that asks whether you want to use a video file or take a video
-  //It will have two buttons that we need functionality for: UploadFile and TakeAVideo
-
-  $cordovaFile.getFreeDiskSpace().then(function (success) {
-    // success in kilobytes
-  }, function (error) {
-    // error
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
   });
 
-  $scope.on_uploadFile = function(){
-    console.log("clicked");
-// TODO: Upload File Functionality
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
 
-    $cordovaFile.checkDir(cordova.file.files, externalDataDirectory)
-      .then(function (success) {
-        // success
-      }, function (error) {
-        // error
-      });
-}
-
-$scope.on_takeAVideo = function(){
-
-  $state.go('takeAVideoPage');
-
-  //Save it as a file (idk what type is easiest)
-  //Use this video file as the template to edit in the rest of app
-  }
-})
-
+});
